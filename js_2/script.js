e4f87e8fec515138623bceec38b02c513cee6f03
mouse_type <denisshashko1523@gmail.com>
2023-10-02 18:46:30 +0300

    added js_2

---
+++ js_2/script.js
@@ -0,0 +1,5 @@
+'use strict'
+function click() {
+    var element = document.getElementsByClassName('className').innerHTML = '<h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h1>';
+}
+click();
